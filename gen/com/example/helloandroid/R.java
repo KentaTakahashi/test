3cffd10f0e80f3f69634ba534d4af54514c8a512
mainPC <User@Gaming-PC>
2013-01-27 22:03:15 +0900

    数字表示まで完了 ButtonオブジェクトをimageButtonに修正した

--- gen/com/example/helloandroid/R.java
+++ gen/com/example/helloandroid/R.java
@@ -11,23 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int num01=0x7f020002;
-        public static final int num02=0x7f020003;
-        public static final int num03=0x7f020004;
-        public static final int num04=0x7f020005;
-        public static final int num05=0x7f020006;
-        public static final int num06=0x7f020007;
-        public static final int num07=0x7f020008;
-        public static final int num08=0x7f020009;
-        public static final int num09=0x7f02000a;
-        public static final int num10=0x7f02000b;
-        public static final int num11=0x7f02000c;
-        public static final int num12=0x7f02000d;
-        public static final int num13=0x7f02000e;
-        public static final int num14=0x7f02000f;
-        public static final int num15=0x7f020010;
+        public static final int blank=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int num01=0x7f020003;
+        public static final int num02=0x7f020004;
+        public static final int num03=0x7f020005;
+        public static final int num04=0x7f020006;
+        public static final int num05=0x7f020007;
+        public static final int num06=0x7f020008;
+        public static final int num07=0x7f020009;
+        public static final int num08=0x7f02000a;
+        public static final int num09=0x7f02000b;
+        public static final int num10=0x7f02000c;
+        public static final int num11=0x7f02000d;
+        public static final int num12=0x7f02000e;
+        public static final int num13=0x7f02000f;
+        public static final int num14=0x7f020010;
+        public static final int num15=0x7f020011;
     }
     public static final class id {
         public static final int Button01=0x7f070001;
--- src/com/example/helloandroid/NumericPuzzle.java
+++ src/com/example/helloandroid/NumericPuzzle.java
@@ -4,18 +4,68 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ImageButton;
 
 public class NumericPuzzle extends Activity {
+
+static final int imageButtons[] = {
+	R.id.Button01,
+	R.id.Button02,
+	R.id.Button03,
+	R.id.Button04,
+	R.id.Button05,
+	R.id.Button06,
+	R.id.Button07,
+	R.id.Button08,
+	R.id.Button09,
+	R.id.Button10,
+	R.id.Button11,
+	R.id.Button12,
+	R.id.Button13,
+	R.id.Button14,
+	R.id.Button15,
+	R.id.Button16
+	};
+
+static final int numImages[] = {
+	R.drawable.num01,
+	R.drawable.num02,
+	R.drawable.num03,
+	R.drawable.num04,
+	R.drawable.num05,
+	R.drawable.num06,
+	R.drawable.num07,
+	R.drawable.num08,
+	R.drawable.num09,
+	R.drawable.num10,
+	R.drawable.num11,
+	R.drawable.num12,
+	R.drawable.num13,
+	R.drawable.num14,
+	R.drawable.num15,
+	R.drawable.blank
+	};
+
+private boolean gameStarted = false;
+
+OrderController orders[] = new OrderController[imageButtons.length];
 
 	//called activity is first created
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
+		createOrderController();
 		setStartButtonListener();
+	}
+	private void createOrderController() {
+		for(int i = 0; i < imageButtons.length; i++) {
+			ImageButton imgbtn = (ImageButton) findViewById(imageButtons[i]);
+			orders[i] = new OrderController(imgbtn, i, numImages[i]);
+		}
 	}
 	private void startChronometer() {
 		Chronometer chrono = (Chronometer)findViewById(R.id.chronometer1);
@@ -25,6 +75,7 @@ public class NumericPuzzle extends Activity {
 
 	private void setStartButtonListener(){
 		Button btn = (Button)findViewById(R.id.start_btn);
+
 		btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v){
 				//startGame();
@@ -33,5 +84,63 @@ public class NumericPuzzle extends Activity {
 		});
 
 
+	}
+
+
+	private void complete() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	private boolean isCompleted() {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	private void searchDir(int idx2) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	class OrderController implements OnClickListener {
+
+		ImageButton imgBtn;
+		int idx = 0;
+		int curImageID = 0;
+
+		public OrderController(ImageButton ibtn, int i, int resid) {
+			imgBtn = ibtn;
+			idx = i;
+			setImageRes(resid);
+			imgBtn.setOnClickListener(this);
+		}
+
+		public int setImageRes(int resid) {
+			int old = curImageID;
+			imgBtn.setImageResource(resid);
+			return old;
+		}
+
+		public int getImageRes() {
+			return curImageID;
+		}
+
+		@Override
+		public void onClick(View v) {
+			if(curImageID == R.drawable.blank){
+				return;
+			}
+			searchDir(idx);
+
+			if(isCompleted()){
+				complete();
+			}
+		}
+
+
+		public void swapImage(OrderController other) {
+			int Previous = other.setImageRes(curImageID);
+			setImageRes(Previous);
+		}
 	}
 }
