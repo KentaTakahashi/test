542d2afcb018da95ecfa89756b7ce626f742e815
mainPC <User@Gaming-PC>
2013-02-16 15:38:35 +0900

    graphics.Matrixによる描画画像の静的回転を実装

--- ImageViewer/src/com/example/imageviewer/MainActivity.java
+++ ImageViewer/src/com/example/imageviewer/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ImageView;
@@ -19,8 +20,19 @@ public class MainActivity extends Activity {
 		ImageView imgView = (ImageView) findViewById(R.id.imageView1);
 		//表示画像取得
 		Resources r = getResources();
-        Bitmap srcBitmap = BitmapFactory.decodeResource(r, R.drawable.num01);
-        imgView.setImageBitmap(srcBitmap);
+		Bitmap srcBitmap = BitmapFactory.decodeResource(r, R.drawable.num01);
+
+		//ref http://ankohouse.sblo.jp/article/56905045.html
+		int deg = 90;//カンパス回転角度
+		Matrix mat = new Matrix();//画像に対しての行列操作可能なクラス、createBitmap時にパラメータとして与える
+		mat.postRotate(deg);//deg度の回転行列に変換
+
+		int sw = srcBitmap.getWidth();
+		int sh = srcBitmap.getHeight();
+		Bitmap rotatedBitmap = Bitmap.createBitmap(srcBitmap, 0, 0, sw, sh, mat, true);
+
+		//imgView.setImageBitmap(srcBitmap);
+		imgView.setImageBitmap(rotatedBitmap);
 	}
 
 	@Override
