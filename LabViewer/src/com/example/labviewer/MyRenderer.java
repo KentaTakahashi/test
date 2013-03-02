6987cc067bdd85a022d7f3a870fa1486cda1c415
mainPC <User@Gaming-PC>
2013-03-02 12:27:48 +0900

    OpenGL Study2 http://android.keicode.com/basics/opengl-lighting.php

--- LabViewer/src/com/example/labviewer/MyRenderer.java
+++ LabViewer/src/com/example/labviewer/MyRenderer.java
@@ -15,10 +15,19 @@ public class MyRenderer implements Renderer {
 		// TODO 自動生成されたメソッド・スタブ
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
+
+
 		gl.glMatrixMode(GL10.GL_MODELVIEW);
 		gl.glLoadIdentity();
 		gl.glTranslatef(0, 0, -3f);
+
+		//Frameを回転
+		gl.glRotatef(30f, 0, 1, 0);
+	    //gl.glRotatef(30f, 1, 0, 0);
+
 		myCube.draw(gl);
+
+
 	}
 
 	@Override
@@ -31,6 +40,10 @@ public class MyRenderer implements Renderer {
 	    gl.glMatrixMode(GL10.GL_PROJECTION);
 	    gl.glLoadIdentity();
 	    GLU.gluPerspective(gl, 45f,(float) width / height, 1f, 50f);
+
+	    //ライティングを有効化
+	    gl.glEnable(GL10.GL_LIGHTING);
+	    gl.glEnable(GL10.GL_LIGHT0);
 	}
 
 	@Override
