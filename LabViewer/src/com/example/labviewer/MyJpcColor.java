55a182096561858571aaf73d626ef1e73b220300
mainPC <User@Gaming-PC>
2013-03-02 19:44:50 +0900

    JPCガマット表示完了

---
+++ LabViewer/src/com/example/labviewer/MyJpcColor.java
@@ -0,0 +1,101 @@
+package com.example.labviewer;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyJpcColor {
+
+	private final FloatBuffer mVertexBuffer;
+	private final IntBuffer mColorBuffer;
+
+	public MyJpcColor(){
+
+		float vertices[] = {
+
+				//a→X, L→Y, b→Z の対応となるので要注意!
+
+				// ハイライト側の6角推
+				0.005f, 0.93f * 2.0f - 1.0f, 	0.004f,	// W 白のみJPC2007に記載が無かったため決めうち
+				0.69f,	0.46f * 2.0f - 1.0f,	0.42f,	// R
+				-0.06f,	0.88f * 2.0f - 1.0f,	0.92f,	// Y
+				-0.71f,	0.50f * 2.0f - 1.0f,	0.25f,	// G
+				-0.39f,	0.55f * 2.0f - 1.0f,	-0.49f,	// C
+				0.18f,	0.23f * 2.0f - 1.0f,	-0.47f,	// B
+				0.75f,	0.46f * 2.0f - 1.0f,	-0.06f,	// M
+				0.69f,	0.46f * 2.0f - 1.0f,	0.42f,	// R
+
+				// シャドー側の6角推
+				0.01f,	0.14f * 2.0f - 1.0f,	0.01f,	// Bk
+				0.69f,	0.46f * 2.0f - 1.0f,	0.42f,	// R
+				-0.06f,	0.88f * 2.0f - 1.0f,	0.92f,	// Y
+				-0.71f,	0.50f * 2.0f - 1.0f,	0.25f,	// G
+				-0.39f,	0.55f * 2.0f - 1.0f,	-0.49f,	// C
+				0.18f,	0.23f * 2.0f - 1.0f,	-0.47f,	// B
+				0.75f,	0.46f * 2.0f - 1.0f,	-0.06f,	// M
+				0.69f,	0.46f * 2.0f - 1.0f,	0.42f,	// R
+		};
+
+		ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+		vbb.order(ByteOrder.nativeOrder());
+		mVertexBuffer = vbb.asFloatBuffer();
+		mVertexBuffer.put(vertices);
+		mVertexBuffer.position(0);
+
+		int one = 0x10000;
+		int colors[] = {
+
+				// ハイライト側の6角推
+				one,	one,	one,	one,	// W
+				one,	0,		0,		one,	// R
+				one,	one,	0,		one,	// Y
+				0,		one,	0,		one,	// G
+				0,		one,	one,	one,	// C
+				0,		0,		one,	one,	// B
+				one,	0,		one,	one,	// M
+				one,	0,		0,		one,	// R
+
+				// シャドー側の6角推
+				0,		0,		0,		one,	// Bk
+				one,	0,		0,		one,	// R
+				one,	one,	0,		one,	// Y
+				0,		one,	0,		one,	// G
+				0,		one,	one,	one,	// C
+				0,		0,		one,	one,	// B
+				one,	0,		one,	one,	// M
+				one,	0,		0,		one,	// R
+		};
+
+		ByteBuffer vbb2 = ByteBuffer.allocateDirect(colors.length * 4);
+		vbb2.order(ByteOrder.nativeOrder());
+		mColorBuffer = vbb2.asIntBuffer();
+		mColorBuffer.put(colors);
+		mColorBuffer.position(0);
+	}
+	public void draw(GL10 gl){
+
+		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+
+		gl.glColorPointer(4, GL10.GL_FIXED, 0, mColorBuffer);
+		gl.glEnableClientState(GL10.GL_COLOR_ARRAY );
+
+		// Front
+	    gl.glNormal3f(0, 0, 1.0f);
+	    gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 3);
+
+
+		// ハイライト側の6角推
+		gl.glNormal3f(0, 0, 1.0f);
+		gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 0, 8);//連続した三角形で扇形を描きます。http://www.komoto.org/opengl/sample01.html
+
+		// シャドー側の6角推
+		gl.glNormal3f(0, 0, 1.0f);
+		gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 8, 8);
+
+
+	}
+}
--- LabViewer/src/com/example/labviewer/MyRenderer.java
+++ LabViewer/src/com/example/labviewer/MyRenderer.java
@@ -9,24 +9,31 @@ import android.opengl.GLU;
 public class MyRenderer implements Renderer {
 
 	MyCube myCube = new MyCube();
+	MyJpcColor myJpcColor  = new MyJpcColor();
+
+	private int size = 0x10000;
+	Triangle mTriangle  = new Triangle(0, size);
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		// TODO 自動生成されたメソッド・スタブ
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
-
-
 		gl.glMatrixMode(GL10.GL_MODELVIEW);
 		gl.glLoadIdentity();
 		gl.glTranslatef(0, 0, -3f);
 
+		//ライティングを有効化
+	    //gl.glEnable(GL10.GL_LIGHTING);
+	    //gl.glEnable(GL10.GL_LIGHT0);
+
 		//Frameを回転
-		gl.glRotatef(30f, 0, 1, 0);
+		gl.glRotatef(30f, 1, 1, 0);
 	    //gl.glRotatef(30f, 1, 0, 0);
 
-		myCube.draw(gl);
-
+		//myCube.draw(gl);
+		myJpcColor.draw(gl);
+		//mTriangle.draw(gl);
 
 	}
 
@@ -41,16 +48,42 @@ public class MyRenderer implements Renderer {
 	    gl.glLoadIdentity();
 	    GLU.gluPerspective(gl, 45f,(float) width / height, 1f, 50f);
 
-	    //ライティングを有効化
-	    gl.glEnable(GL10.GL_LIGHTING);
-	    gl.glEnable(GL10.GL_LIGHT0);
 	}
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		// TODO 自動生成されたメソッド・スタブ
+		//デプステストを有効。3D描画時に利用。デプスバッファが浅いものが上に表示される。
 		gl.glEnable(GL10.GL_DEPTH_TEST);
 	    gl.glDepthFunc(GL10.GL_LEQUAL);
+	    /**
+	     * デプスバッファの評価方は glDepthFunc を使用します。
+	     * 初期値は GL_LESS であり、
+		 *新たな値がバッファの値より小さければ表示される。
+		 *すなわち近いものほど上に描画される設定となる。
+
+		 *GL_NEVER:全て採用されない
+		 *GL_LESS:バッファより値が小さければ採用
+		 *GL_LEQUAL:バッファの値以下であれば採用
+		 *GL_EQUAL:バッファの値と同じであれば採用
+		 *GL_GREATER:バッファより値が大きければ採用
+		 *GL_NOTEQUAL:バッファの値と同じでなければ採用
+		 *GL_GEQUAL:バッファの値以上であれば採用
+		 *GL_ALWAYS:常に採用される
+	     */
+
+	    /**
+	    *  DITHERをオフにします。
+	    *  DITHERとは量子化の誤差を最小にするべく
+	    *  サンプルデータに意図的に追加される誤った信号・データのこと。
+	    */
+	    gl.glDisable(GL10.GL_DITHER);
+	    // 背景色
+	    gl.glClearColor(1,1,1,1);
+	    //スムースシェーディング：平面のポリゴンを曲面に見せかける処理。
+	    gl.glShadeModel(GL10.GL_SMOOTH);
+
+
 	}
 
 }
