d7bcd210c98040231a34e0c11937726e3d1b615a
mainPC <User@Gaming-PC>
2013-06-29 17:36:49 +0900

    first commit

---
+++ Jave Exercises/src/JPL/ch01/ex01_01/HelloWorld.java
@@ -0,0 +1,13 @@
+package JPL.ch01.ex01_01;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello, world");
+	}
+
+}
