fcf4985edfb707f040ed9ea18173752ece12b088
z00s000704 <z00s000704@ZRFG021078.rfg.rfgad.ricoh.com>
2013-07-17 09:01:36 +0900

    ex02_11まで完了

---
+++ Jave Exercises/src/高橋健太/JPL/ch02/ex02_11/Vehicle.java
@@ -0,0 +1,54 @@
+package 高橋健太.JPL.ch02.ex02_11;
+
+public class Vehicle {
+
+
+	static int nextID = 0;
+
+	private int mID;
+	private int mSpeed;
+	private int mDirection;
+	private String mOwner;
+
+	public Vehicle() {
+		mID = nextID;
+		nextID++;
+
+		mOwner = "Alice";//デフォルトのオーナー
+	}
+
+	public Vehicle(String owner) {
+		mID = nextID;
+		nextID++;
+
+		mOwner = owner;
+	}
+	static public int getMaxID() {
+		return nextID - 1;
+	}
+	static public void setID(int id) {
+		nextID = id;
+	}
+	public int getID() {
+		return mID;
+	}
+	public int getSpeed() {
+		return mSpeed;
+	}
+	public int getDirection() {
+		return mDirection;
+	}
+	public String getOwner() {
+		return mOwner;
+	}
+	//ID,Speed,Direction,Ownerを連結した文字列を返す
+	public String toString() {
+		String str =
+				"ID:"			+ Integer.toString(mID) +
+				", Speed:"		+ Integer.toString(mSpeed) +
+				", Direction:"	+ Integer.toString(mDirection) +
+				", Owner:"		+ mOwner;
+
+		return str;
+	}
+}
