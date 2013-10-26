953f38bf0d4618d2d5e975f3c86068c522211928
mainPC <User@Gaming-PC>
2013-10-26 19:08:27 +0900

    ex14_10完了

--- Jave Exercises/src/高橋健太/JPL/ch14/ex14_10/TestThreadPool.java
+++ Jave Exercises/src/高橋健太/JPL/ch14/ex14_10/TestThreadPool.java
@@ -1,3 +1,4 @@
+package 高橋健太.JPL.ch14.ex14_10;
 /*
  * Copyright (C) 2012, 2013 RICOH Co., Ltd. All rights reserved.
  */
@@ -22,13 +23,13 @@ public class TestThreadPool {
 	 */
 	private static class CounterTask implements Runnable {
 		private int runCount = 0;
-		
+
 		@Override
 		public synchronized void run() {
 			runCount++;
 			notifyAll();
 		}
-		
+
 		synchronized int waitForRunCount(int count) {
 			while (this.runCount < count) {
 				try {
@@ -40,7 +41,7 @@ public class TestThreadPool {
 			return runCount;
 		}
 	}
-	
+
 	/**
 	 * A simple latch task whose run() method will wait for other threads would
 	 * execute the run() method until the expected number of run() invocations reached.
@@ -48,11 +49,11 @@ public class TestThreadPool {
 	private static class LatchTask implements Runnable {
 		private final int latchCount;
 		private int currentCount = 0;
-		
+
 		LatchTask(int count) {
 			this.latchCount = count;
 		}
-		
+
 		public synchronized void run() {
 			currentCount++;
 			notifyAll();
@@ -64,7 +65,7 @@ public class TestThreadPool {
 				}
 			}
 		}
-		
+
 		synchronized void waitForLatchCount() {
 			while (currentCount < latchCount) {
 				try {
@@ -75,44 +76,44 @@ public class TestThreadPool {
 			}
 		}
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstructorIllegalArgumentFirst() {
 		new ThreadPool(0, 1);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstructorIllegalArgumentSecond() {
 		new ThreadPool(1, 0);
 	}
-	
+
 	@Test
 	public void testStartAndStop() {
 		ThreadPool tp = new ThreadPool(1,1);
 		tp.start();
 		tp.stop();
 	}
-	
+
 	@Test(expected=IllegalStateException.class)
 	public void testStopBeforeStart() {
 		ThreadPool tp = new ThreadPool(1,1);
 		tp.stop();
 	}
-	
+
 	@Test(expected=IllegalStateException.class)
 	public void testRestartWithoutStop() {
 		ThreadPool tp = new ThreadPool(1,1);
 		tp.start();
 		tp.start();
 	}
-	
+
 	@Test(expected=NullPointerException.class)
 	public void testDispatchNullArgument() {
 		ThreadPool tp = new ThreadPool(1,1);
 		tp.start();
 		tp.dispatch(null);
 	}
-	
+
 	@Test(expected=IllegalStateException.class)
 	public void testDispatchBeforeStart() {
 		ThreadPool tp = new ThreadPool(1,1);
@@ -128,33 +129,33 @@ public class TestThreadPool {
 		t.waitForRunCount(1);
 		tp.stop();
 	}
-	
+
 	@Test
 	public void testSimpleRepeatedDispatch() {
 		ThreadPool tp = new ThreadPool(1,1);
 		tp.start();
 		CounterTask t = new CounterTask();
-		
+
 		for (int i = 0; i < 10; i++)
 			tp.dispatch(t);
-		
+
 		t.waitForRunCount(10);
 		tp.stop();
 	}
-	
+
 	@Test
 	public void testComplexRepeatedDispatch() {
 		ThreadPool tp = new ThreadPool(10,10);
 		tp.start();
 		CounterTask t = new CounterTask();
-		
+
 		for (int i = 0; i < 1000; i++)
 			tp.dispatch(t);
-		
+
 		t.waitForRunCount(1000);
 		tp.stop();
 	}
-	
+
 	@Test
 	public void testComplexRepeatedDispatch2() {
 		ThreadPool tp = new ThreadPool(10,10);
@@ -162,18 +163,18 @@ public class TestThreadPool {
 		CounterTask[] tasks = new CounterTask[10];
 		for (int i = 0; i < tasks.length; i++)
 			tasks[i] = new CounterTask();
-		
+
 		for (int i = 0; i < 100; i++) {
 			for (CounterTask t: tasks)
 				tp.dispatch(t);
 		}
-		
+
 		for (CounterTask t: tasks)
 			t.waitForRunCount(100);
-				
+
 		tp.stop();
 	}
-	
+
 	@Test
 	public void testLatchSimpleDispatch() {
 		final int numberOfThreads = 10;
@@ -183,45 +184,45 @@ public class TestThreadPool {
 
 		for (int i = 0; i < numberOfThreads; i++)
 			tp.dispatch(t);
-		
+
 		t.waitForLatchCount();
 		tp.stop();
 	}
-	
+
 	@Test
 	public void testQueueSize() {
-		
+
 		final int sizeOfQueue = 10;
 		ThreadPool tp = new ThreadPool(sizeOfQueue, 1);
 		tp.start();
-		
+
 		// How do I implement this test method ?
-		
+
 		tp.stop();
 	}
-	
+
 	@Test
 	public void testLatchComplexDispatch() {
 		final int numberOfThreads = 10;
 		ThreadPool tp = new ThreadPool(10,numberOfThreads);
 		tp.start();
-		
+
 		LatchTask[] tasks = new LatchTask[10];
-		for (int i = 0; i < tasks.length; i++) 
+		for (int i = 0; i < tasks.length; i++)
 			tasks[i] = new LatchTask(numberOfThreads);
-		
+
 		for (LatchTask t: tasks) {
 			for (int i = 0; i < numberOfThreads; i++) {
 				tp.dispatch(t);
 			}
 		}
-		
+
 		for (LatchTask t: tasks)
 			t.waitForLatchCount();
-				
+
 		tp.stop();
 	}
-	
+
 	@Test
 	public void testNumberOfThreads() {
 		final Set<Thread> threads = Collections.synchronizedSet(new HashSet<Thread>());
@@ -236,23 +237,23 @@ public class TestThreadPool {
 				}
 			}
 		};
-		
+
 		final int numberOfThreads = 10;
 		ThreadPool tp = new ThreadPool(10,numberOfThreads);
 		tp.start();
 		for (int i = 0; i < numberOfThreads; i++)
 			tp.dispatch(task);
-		
+
 		// By the specification, stop() will wait for the terminations of all threads.
 		tp.stop();
-		
+
 		assertEquals(numberOfThreads, threads.size());
 	}
-	
+
 	@Test
 	public void testTerminationOfThreads() {
 		final List<Thread> threads = Collections.synchronizedList(new ArrayList<Thread>());
-		
+
 		Runnable task = new Runnable() {
 			@Override
 			public void run() {
@@ -268,7 +269,7 @@ public class TestThreadPool {
 				}
 			}
 		};
-		
+
 		final int numberOfThreads = 10;
 		ThreadPool tp = new ThreadPool(10,numberOfThreads);
 		tp.start();
@@ -276,7 +277,7 @@ public class TestThreadPool {
 			tp.dispatch(task);
 		// By the specification, stop() will wait for the terminations of all threads.
 		tp.stop();
-		
+
 		assertEquals(numberOfThreads, threads.size());
 		for (Thread t: threads)
 			assertFalse(t.isAlive());
--- Jave Exercises/src/高橋健太/JPL/ch14/ex14_10/ThreadPool.java
+++ Jave Exercises/src/高橋健太/JPL/ch14/ex14_10/ThreadPool.java
@@ -1,3 +1,7 @@
+package 高橋健太.JPL.ch14.ex14_10;
+
+import java.util.LinkedList;
+
 /*
  * Copyright (C) 2012, 2013 RICOH Co., Ltd. All rights reserved.
  */
@@ -10,7 +14,7 @@
  *
  * [Instruction]
  *  Implement one constructor and three methods.
- *  Don't forget to write a Test program to test this class. 
+ *  Don't forget to write a Test program to test this class.
  *  Pay attention to @throws tags in the javadoc.
  *  If needed, you can put "synchronized" keyword to methods.
  *  All classes for implementation must be private inside this class.
@@ -18,19 +22,34 @@
  */
 public class ThreadPool {
 
+	private final int numberOfThreads;
+	private final threadPoolTask[] myThreads;
+	private final LinkedList<Runnable> queue;
+	private boolean isRun;
 
-	
-    /**
-     * Constructs ThreadPool.
-     *
-     * @param queueSize the max size of queue
-     * @param numberOfThreads the number of threads in this pool.
-     *
-     * @throws IllegalArgumentException if either queueSize or numberOfThreads
-     *         is less than 1
-     */
+	/**
+	 * Constructs ThreadPool.
+	 *
+	 * @param queueSize the max size of queue
+	 * @param numberOfThreads the number of threads in this pool.
+	 *
+	 * @throws IllegalArgumentException if either queueSize or numberOfThreads
+	 *         is less than 1
+	 */
     public ThreadPool(int queueSize, int numberOfThreads) {
-    	throw new AssertionError("Not Implemented Yet");
+
+    	if((queueSize < 1) || (numberOfThreads < 1))
+    		throw new IllegalArgumentException("queueSize or numberOfThreads is less than 1 !");
+
+    	this.numberOfThreads = numberOfThreads;
+
+    	queue = new LinkedList<Runnable>();
+    	myThreads = new threadPoolTask[numberOfThreads];
+    	for (int i = 0; i < numberOfThreads; i++) {
+        	myThreads[i] = new threadPoolTask();
+        }
+
+    	isRun = false;//set start flag false
     }
 
     /**
@@ -39,29 +58,88 @@ public class ThreadPool {
      * @throws IllegalStateException if threads has been already started.
      */
     public void start() {
-       	throw new AssertionError("Not Implemented Yet");
-    }   
+    	if(isRun == true)
+    		throw new IllegalStateException("threads has been already started !");
+    	else {
+    		isRun = true;
+    		for (int i = 0; i < numberOfThreads; i++) {
+    			myThreads[i].start();
+    		}
+    	}
+    }
 
-    /**
-     * Stop all threads and wait for their terminations.
-     *
-     * @throws IllegalStateException if threads has not been started.
-     */
+	/**
+	 * Stop all threads and wait for their terminations.
+	 *
+	 * @throws IllegalStateException if threads has not been started.
+	 */
     public void stop() {
-       	throw new AssertionError("Not Implemented Yet");
+    	if(isRun == false)
+    		throw new IllegalStateException("threads has not been started !");
+    	else {
+    		isRun =false;
+
+    		for (int i = 0; i < numberOfThreads; i++) {
+    			while(myThreads[i].isAlive()){
+    				//wait for terminations
+    			}
+            }
+    	}
     }
 
-    /**
-     * Executes the specified Runnable object, using a thread in the pool.
-     * run() method will be invoked in the thread. If the queue is full, then
-     * this method invocation will be blocked until the queue is not full.
-     * 
-     * @param runnable Runnable object whose run() method will be invoked.
-     *
-     * @throws NullPointerException if runnable is null.
-     * @throws IllegalStateException if this pool has not been started yet.
-     */
-    public synchronized void dispatch(Runnable runnable) {
-       	throw new AssertionError("Not Implemented Yet");
-    }
+	/**
+	 * Executes the specified Runnable object, using a thread in the pool.
+	 * run() method will be invoked in the thread. If the queue is full, then
+	 * this method invocation will be blocked until the queue is not full.
+	 *
+	 * @param runnable Runnable object whose run() method will be invoked.
+	 *
+	 * @throws NullPointerException if runnable is null.
+	 * @throws IllegalStateException if this pool has not been started yet.
+	 */
+	public synchronized void dispatch(Runnable runnable) {
+		if(runnable == null)
+			throw new NullPointerException("runnable param is null !");
+		if(isRun == false)
+			throw new IllegalStateException("threads has not been started !");
+
+		synchronized(queue) {
+			queue.addLast(runnable);
+			queue.notifyAll();
+		}
+	}
+
+	/**
+	 * Simple counter task which counts the number of invocation of run() method.
+	 */
+	private class threadPoolTask extends Thread {
+
+		@Override
+		public void run() {
+			Runnable r;
+
+			outside: while (true) {
+				//if(!isRun) continue;
+				synchronized(queue) {
+					while (queue.isEmpty()) {
+						if(!isRun) break outside;//stop関数が呼ばれたら、outsideのwhile文を抜ける
+						try {
+							queue.wait(10);//10msec wait
+						} catch (InterruptedException ignored) {
+					    //何もしない
+						}
+					}
+					r = (Runnable) queue.removeFirst();
+				}
+				// If we don't catch RuntimeException, // the pool could leak threads
+				try {
+					r.run();
+				}
+				catch (RuntimeException e) {
+					// You might want to log something here
+				}
+				if(!isRun) break outside;//stop関数が呼ばれたら、outsideのwhile文を抜ける
+			}
+		}
+	}
 }
