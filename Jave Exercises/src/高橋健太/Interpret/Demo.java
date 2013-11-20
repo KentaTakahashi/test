3d695c9cc429055fdaaf043b7b616f4a61953f05
z00s000704 <z00s000704@ZRFG021078.rfg.rfgad.ricoh.com>
2013-11-20 14:58:58 +0900

    InterpretのDemoクラス一部修正

--- Jave Exercises/src/高橋健太/Interpret/Demo.java
+++ Jave Exercises/src/高橋健太/Interpret/Demo.java
@@ -3,6 +3,8 @@ package 高橋健太.Interpret;
 public class Demo {
 	private final String private_final= "for private final test";
 
+	public Demo(){}
+
 	public Demo(String str)
 			throws Exception, NullPointerException
 	{
@@ -10,5 +12,9 @@ public class Demo {
 		if(str.equals("null"))throw new NullPointerException();
 
 		throw new Exception();
+	}
+
+	private final void privateFinalMethod() {
+		new MessageDialog("called private final method", null);
 	}
 }
