dfdec871ce9611113784f76b0e545ce15001d951
z00s000704 <z00s000704@ZRFG021078.rfg.rfgad.ricoh.com>
2014-02-17 17:47:47 +0900

    ex_23_02完了(ex_23_02は途中)

---
+++ Jave Exercises/src/高橋健太/JPL/ch23/ex23_02/ChildeProg.java
@@ -0,0 +1,13 @@
+package 高橋健太.JPL.ch23.ex23_02;
+
+public class ChildeProg {
+	public static void main(String[] args) {
+		System.out.println("Start ChildeProg Process");
+		System.out.println("Running ChildeProg Process");
+		System.out.println("Running ChildeProg Process");
+		System.out.println("Running ChildeProg Process");
+		System.out.println("Running ChildeProg Process");
+		System.out.println("Running ChildeProg Process");
+		System.out.println("Finish ChildeProg Process");
+	}
+}
